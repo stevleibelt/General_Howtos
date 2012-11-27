26d0d9e8971f9cc671b924f317784e227030d009
Max Mustermann <max.mustermann@jobleads.de>
2012-11-27 10:28:31 +0100

    Added switch/case as example.

--- bash/parameters.sh
+++ bash/parameters.sh
@@ -10,24 +10,30 @@
 
 # check is variable is null/zero
 if [ -z "$1" ]; then 
-  echo "\$1 is null"
+  echo "\$1 is null."
 fi
 
 # check if variable is not null
 if [ -n "$1" ]; then
-  echo "\$1 is not null"
+  echo "\$1 is not null."
 fi
 
 # check if variable is not an empty string
 if [ "$1" != "" ]; then
-  echo "\$1 is set"
+  echo "\$1 is set/not blank."
 else
-  echo "\$1 is not set"
+  echo "\$1 is not set/is blank."
 fi
 
 # check the number of given parameters
 if [ "$#" -gt 0 ]; then
-  echo "$# parameters are given"
+  echo "$# parameters are given."
 else
-  echo "No parameter is given"
+  echo "No parameter is given."
 fi
+
+# checks combined in a case
+case "$1" in
+ "") echo "First parameter is blank.";;
+ *) echo "First parameter is not blank."
+esac
