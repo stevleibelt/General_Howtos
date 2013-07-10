e2ffa090805c89c43fb7c042ef591593140031b1
stevleibelt <stev@leibelt.de>
2013-07-10 16:35:20 +0200

    Added if example in select stagement

--- mysql/select.sql
+++ mysql/select.sql
@@ -1,4 +1,8 @@
 -- some usefull select statements
 -- 
+
 -- create md5 based on current unix timestamp
 SELECT md5(UNIX_TIMESTAMP());
+
+-- if you need to display text
+SELECT IF(`my_column` IS NULL, 'no', 'yes') AS `Status`;
