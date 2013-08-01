9b7965cb7ba19b74560709c0b49ca0124d10d727
stevleibelt <stev@leibelt.de>
2013-08-01 16:31:03 +0200

    Added complex update with join

--- mysql/update.sql
+++ mysql/update.sql
@@ -40,3 +40,16 @@ SET
 WHERE 
     `my_table_one`.`column_two` = 'foobar',
     AND `my_table_two`.`column_two` = 'foobar';
+
+--- update multiple and joined tables
+-- http://stackoverflow.com/questions/806882/update-multiple-tables-in-mysql-using-left-join
+UPDATE 
+    `my_table_one` AS `t1`
+JOIN
+    `my_table_two` AS `t2`
+    ON `t1`.`t2_id` = `t2`.`id`
+JOIN
+    `my_table_three` AS `t3`
+    ON `t1`.`t3_id` = `t3`.`id`
+SET 
+    `t1`.`columne_one` = `t3`.`column_one`
