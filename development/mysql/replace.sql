a38dbd2cf3796f0a313f2e21d9ae4037129b74ec
stevleibelt <stev@leibelt.de>
2013-10-29 12:40:42 +0100

    Added replace

--- development/mysql/replace.sql
+++ development/mysql/replace.sql
@@ -5,4 +5,11 @@ REPLACE INTO
 VALUES (
   'foo', 
   23
-)
+);
+
+-- update and replace parts of the string
+
+UPDATE 
+    `table_one`
+SET 
+    `my_column` = REPLACE(`my_column`, 'search_string', 'replace_string');
