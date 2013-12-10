d13abd6aacf13c3142b7fed8ce252618f2dbeb0e
stevleibelt <stev@leibelt.de>
2013-12-10 09:54:28 +0100

    added concat example

---
+++ development/mysql/concat.sql
@@ -0,0 +1,17 @@
+-- 
+-- based on: http://dev.mysql.com/doc/refman/5.0/en/string-functions.html#function_concat
+-- 
+
+-- concat strings
+
+SELECT 
+    CONCAT (`column_one`, `column_two`) 
+FROM
+    `my_table`
+
+-- concat strings with separator
+
+SELECT 
+    CONCAT_WS ('my_separator', `column_one`, `column_two`) 
+FROM
+    `my_table`
