20ba448cfe47a6177a3ee8c640d11e3ab5ed1097
stevleibelt <stev@leibelt.de>
2014-02-13 20:06:06 +0100

    added author

--- development/cplusplus/basic/helloWorld/main.cpp
+++ development/cplusplus/basic/helloWorld/main.cpp
@@ -1,3 +1,8 @@
+/*
+ * @author artodeto <artodeto@bazzline.net>
+ * @since 2014-02-13
+ */
+
 #include <iostream>
 using namespace std;
 
---
+++ development/cplusplus/basic/inputOutput/main.cpp
@@ -0,0 +1,5 @@
+/**
+ * @author artodeto <artodeto@bazzline.net>
+ * @since 2014-02-13
+ **/
+#include <iostream>
