d12d2a9af5f8114315b7cbc09ac85fa5113626a8
stevleibelt <stev@leibelt.de>
2014-02-13 20:12:11 +0100

    added simple input/output example

--- development/cplusplus/basic/inputOutput/main.cpp
+++ development/cplusplus/basic/inputOutput/main.cpp
@@ -1,5 +1,17 @@
-/**
+/*
  * @author artodeto <artodeto@bazzline.net>
  * @since 2014-02-13
- **/
+ */
+
 #include <iostream>
+using namespace std;
+
+int main() {
+    int input{0};
+
+    cout << "Please input a integer: ";
+    cin >> input;
+    cout << "Your input is: " << input << endl;
+
+    return 0;
+}
