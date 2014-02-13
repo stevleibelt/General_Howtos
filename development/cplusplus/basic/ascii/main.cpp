b234c9ca194d3e86ae6cd3417b35ebc3abddf600
stevleibelt <stev@leibelt.de>
2014-02-13 20:17:07 +0100

    added example for ascii character handling

---
+++ development/cplusplus/basic/ascii/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * @author stev leibelt <artodeto@bazzline.net>
+ * @since 2014-02-13
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char stringA = 'A';
+    char stringB = 'B';
+    char asciiA = 67;
+    char asciiB = 68;
+
+    cout << "stringA: " << stringA << endl;
+    cout << "stringB: " << stringB << endl;
+    cout << "asciiA: " << asciiA << endl;
+    cout << "asciiB: " << asciiB << endl;
+
+    return 0;
+}
