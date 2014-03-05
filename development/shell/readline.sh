7238a74a25ee04789c7414c95096bfcf6e3a4aaf
stevleibelt <stev@leibelt.de>
2014-03-05 11:23:17 +0100

    added

---
+++ development/shell/readline.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+####
+# @author stev leibelt <artodeto@bazzline.net>
+# @since 2014-03-05
+####
+
+echo 'enter something'
+read VALUE
+
+echo 'you have entered something like that: "'$VALUE'"'
