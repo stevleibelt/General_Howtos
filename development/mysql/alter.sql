b541a9e250328045d0ae7238af608e79681c32f5
stevleibelt <stev@leibelt.de>
2014-03-13 14:28:55 +0100

    added alter statement

--- development/mysql/alter.sql
+++ development/mysql/alter.sql
@@ -1,4 +1,5 @@
 -- add column with index
+
 ALTER TABLE 
     `my_table`
 ADD 
@@ -9,6 +10,7 @@ ADD
     INDEX `my_other_column` (`my_other_column`);
 
 -- delete column with index
+
 ALTER TABLE 
     `my_table` 
 DROP 
@@ -19,7 +21,16 @@ DROP
     INDEX `my_other_column`;
 
 -- add index
+
 ALTER TABLE 
     `my_table` 
 ADD 
     INDEX `my_column` (`my_column`);
+
+-- add index on BLOB/TEXT
+-- mySQL: ERROR 1170 (42000): BLOB/TEXT column ‘XXX’ used in key specification without a key length
+
+ALTER TABLE 
+    `my_table`
+ADD
+    INDEX `my_column` (`my_column`(10000)); -- length of the index, to find the longest string, use: SELECT MAX(LENGTH(field_to_query)) FROM table_to_query: SELECT MAX(LENGTH(`my_column`)) FROM `my_table`;
