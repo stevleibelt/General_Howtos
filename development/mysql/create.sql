d2e8647696ab9ee40bb11fd513703a816f565103
stevleibelt <stev@leibelt.de>
2014-03-17 15:27:56 +0100

    added "structure copy" and "copy with data" examples

--- development/mysql/create.sql
+++ development/mysql/create.sql
@@ -3,10 +3,20 @@
 -- https://dev.mysql.com/doc/refman/5.1/en/create-table.html
 -- 
 
-CREATE TABLE [IF NOT EXISTS] `my_table
+CREATE TABLE [IF NOT EXISTS] `my_table`
 (
   `id` INT NOT NULL AUTO_INCREMENT,
   `created_at` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `createdAt` (`created_at`)
 ) Engine=InnoDB DEFAULT CHARSET=utf8 COMMENT='my short comment';
+
+-- copy structure
+
+CREATE TABLE `structure_copy_of_original_table` 
+LIKE `original_table`;
+
+-- copy structure and data
+
+CREATE TABLE `copy_of_original_table`
+SELECT * FROM `original_table`;
