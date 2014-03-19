1107cafd2d7caa7ecb60842df4723c470839ffa2
stevleibelt <stev@leibelt.de>
2014-03-19 09:04:08 +0100

    added disable|enable of foreign key checks

--- development/mysql/foreignKey.sql
+++ development/mysql/foreignKey.sql
@@ -3,6 +3,7 @@
 -- the both keys must have the same data type as well as the same collation (if defined)
 -- 
 -- http://www.1keydata.com/sql/sql-foreign-key.html
+-- http://cristian-radulescu.ro/article/disable-foreign-key-checks-in-mysql.html
 
 CREATE TABLE my_table (
   `id` INT NOT NULL,
@@ -10,3 +11,11 @@ CREATE TABLE my_table (
   PRIMARY KEY (`id`),
   FOREIGN KEY (`other_table_id`) REFERENCES `other_table` (`id`)
 ) ENGINE=InnoDb;
+
+-- disable foreign key check
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- enable foreign key check
+
+SET FOREIGN_KEY_CHECKS = 1;
