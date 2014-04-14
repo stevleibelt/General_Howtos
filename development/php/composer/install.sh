df590b91618780fcaa91027baf48f54b7e089384
stevleibelt <stev@leibelt.de>
2014-04-15 00:15:20 +0200

    added group

--- development/php/composer/install.sh
+++ development/php/composer/install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+groupadd users
+
 cd /tmp
 curl -sS https://getcomposer.org/installer | php
 sudo mkdir -p /opt/php-composer
 sudo mv composer.phar /opt/php-composer
+sudo chgrp -R users /opt/php-composer
+sudo chmod -R g+w /opt/php-composer
 sudo ln -s /opt/php-composer/composer.phar /usr/local/bin/composer
