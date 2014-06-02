bf2f40f499f68f8947d21a12bcc5532e838b8364
stevleibelt <stev@leibelt.de>
2014-06-02 10:37:34 +0200

    added examples

--- development/mysql/subString.sql
+++ development/mysql/subString.sql
@@ -1,4 +1,12 @@
 -- 
+-- https://dev.mysql.com/doc/refman/5.0/en/string-functions.html
+-- https://dev.mysql.com/doc/refman/5.0/en/string-functions.html#function_substr
+-- 
+
+SELECT SUBSTRING('foobarbar', 4); -- 'barbar'
+SELECT SUBSTRING('foobarbar' FROM 4); -- 'barbar'
+
+-- 
 -- assuming you have entries in json format and want to retrieve entries with matching parametervalues
 --
 -- you have a table "my_table_to_search_in" with a text column called "json" where you store a json encoded object/array of parameters
