3cb7c733122afa031525487fa5aa0a9ebb0bbe82
stevleibelt <stev@leibelt.de>
2014-10-16 11:55:41 +0200

    added example

--- development/mysql/importFromFile.sql
+++ development/mysql/importFromFile.sql
@@ -6,6 +6,11 @@ mysql --user=user --password=password --default_character_set utf8 --database=da
 -- import from gzip
 gunzip < $file.gz | mysql --user=user --password=password --default_character_set utf8 --database=database
 
+-- import from file
+-- see
+--  http://www.cyberciti.biz/faq/import-mysql-dumpfile-sql-datafile-into-my-database/
+mysql -u <user name> -p -h <localhost> <database name> < <file name>
+
 -- bypass foreign key chec
 SET foreign_key_checks = 0;
 -- you sql code
