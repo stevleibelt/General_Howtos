1a47cbdfedffafa9553e2fdf6da3305645d1537d
stevleibelt <stev@leibelt.de>
2014-11-05 16:24:02 +0100

    added selectMostRecentDateSelect

---
+++ development/mysql/selectMostRecentDateSelect.sql
@@ -0,0 +1,27 @@
+-- 
+-- select row with most recent date per user
+-- http://stackoverflow.com/questions/17038193/select-row-with-most-recent-date-per-user
+-- 
+
+SELECT 
+    login_data.*
+FROM
+    (
+        SELECT 
+            `login`.`user_id` AS `user_id`
+            MAX(`login`.`last_login`) AS `last_login`
+        FROM
+            `login`
+        GROUP BY
+            `login`.`user_id`
+    ) AS highest_login
+    JOIN (
+        SELECT 
+            * 
+        FROM
+            `login`
+    ) AS login_data
+        ON (
+            highest_login.user_id = login_data.user_id
+            AND highest_login.last_login = login_data. last_login
+        );
