ee0e573f3ec80a8b3f6339b2191d5d0efcbb8089
stevleibelt <stev@leibelt.de>
2015-01-02 21:51:34 +0100

    fixed mail

--- development/javascript/sleep.js
+++ development/javascript/sleep.js
@@ -2,7 +2,7 @@
 /**
  * taken from: http://www.phpied.com/sleep-in-javascript/
  *
- * @author stev leibelt <artodeto@arcor.de>
+ * @author stev leibelt <artodeto@bazzline.net>
  * @since 2013-11-18
  */
 
