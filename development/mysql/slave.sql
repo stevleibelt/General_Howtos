dca11f622d5b6f69b8e59a359fe2d5c265c39cfc
stevleibelt <stev@leibelt.de>
2015-02-16 10:26:17 +0100

    started slave replication section

---
+++ development/mysql/slave.sql
@@ -0,0 +1,9 @@
+-- 
+-- get slave status
+-- must be executed on all slaves
+-- 
+-- @see
+--  https://dev.mysql.com/doc/refman/5.1/en/replication-administration-status.html
+-- 
+
+SHOW SLAVE STATUS;
