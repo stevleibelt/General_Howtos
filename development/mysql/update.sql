dcb8a900d870ca21b1fd4c9c86a79068fbce0d35
stevleibelt <stev@leibelt.de>
2015-04-02 14:40:48 +0200

    Added example with NOT EXISTS

--- development/mysql/update.sql
+++ development/mysql/update.sql
@@ -9,3 +9,16 @@ SET
     [, `my_other_column` = 13]
 WHERE 
     `my_column` <> 666;
+
+-- 
+-- if not exists (easier than a left join)
+-- @see: https://dev.mysql.com/doc/refman/5.0/en/exists-and-not-exists-subqueries.html
+-- 
+
+UPDATE
+    `my_table` 
+SET 
+    `my_column` = 666 
+WHERE 
+    `my_column` <> 666
+    AND NOT EXISTS (SELECT * FROM `my_other_table` WHERE `my_table`.`my_other_table_id` = `my_other_table`.`id`);
