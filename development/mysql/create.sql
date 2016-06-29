3f35977468622862b284d3dcd55fdbaa7bafa2de
stevleibelt <stev@leibelt.de>
2016-06-29 21:34:36 +0200

    added

--- development/mysql/create.sql
+++ development/mysql/create.sql
@@ -25,3 +25,23 @@ SELECT * FROM `original_table`;
 -- autoincrement
 -- @see: https://dev.mysql.com/doc/refman/5.7/en/example-auto-increment.html
 -- "When the column reaches the upper limit of the data type, the next attempt to generate a sequence number fails."
+
+-- 
+-- create a database and grant access to a new created user
+-- @see: http://www.fail2fail.com/archives/52-Create-mysql-database-and-user.html
+-- 
+
+SET @LOCAL_DATABASE_NAME='<your database>';
+SET @LOCAL_USER_NAME='<your user name>';
+SET @LOCAL_USER_PASSWORD='<your user password>';
+
+-- create the database
+CREATE DATABASE @LOCAL_DATABASE_NAME;
+
+-- create user
+CREATE USER @LOCAL_USER_NAME@'localhost' IDENTIFIED BY @LOCAL_USER_PASSWORD;
+
+-- grant user full rights to the database
+GRANT ALL PRIVILEGES ON @LOCAL_DATABASE_NAME.* TO 'user'@'localhost';
+
+FLUSH PRIVILEGES;
