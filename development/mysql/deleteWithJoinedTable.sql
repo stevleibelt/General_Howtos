22c573d001af7746197fdb7df682eb3fc6564412
stevleibelt <stev@leibelt.de>
2016-12-14 22:43:33 +0100

    asd

--- development/mysql/deleteWithJoinedTable.sql
+++ development/mysql/deleteWithJoinedTable.sql
@@ -1,8 +1,9 @@
 -- delete with join
 -- only tables listed in the DELETE are used for deletion
+-- @see: https://dev.mysql.com/doc/refman/5.7/en/delete.html <-- Multi-Table Deletes
 
 DELETE
-    `t1` 
+    `t1` -- you don't need to use alias
 FROM 
     `table_one` AS `t1`
     LEFT JOIN `table_two` AS `t2` 
