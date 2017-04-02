ca922f86771ff126a840d5ef1ba2a2b722d9c730
stevleibelt <stev@leibelt.de>
2017-04-02 10:55:54 +0200

    updated

--- development/mysql/status.sql
+++ development/mysql/status.sql
@@ -1,2 +1,19 @@
 -- for innodb
 SHOW ENGINE INNODB STATUS
+
+-- table status
+-- by using table status
+SHOW 
+    TABLE STATUS 
+FROM 
+    `<database name>`
+WHERE 
+    `name` LIKE '<table name>' ;
+-- the same but with different statement
+SELECT 
+    *
+FROM  
+    INFORMATION_SCHEMA.TABLES
+WHERE 
+    TABLE_SCHEMA = '<database name>'
+    AND TABLE_NAME = '<table name>';
