9882c47445ab9531eda58107e6ac864ee83908dc
stevleibelt <stev@leibelt.de>
2017-09-16 20:14:45 +0200

    updated

--- development/javascript/links.md
+++ development/javascript/links.md
@@ -1,5 +1,6 @@
 # links
 
+* http://www.jsweet.org/
 * http://wiki.commonjs.org/wiki/Promises
 * http://youmightnotneedjs.com/
 * http://youmightnotneedjquery.com/
--- development/mysql/status.sql
+++ development/mysql/status.sql
@@ -17,3 +17,17 @@ FROM
 WHERE 
     TABLE_SCHEMA = '<database name>'
     AND TABLE_NAME = '<table name>';
+
+-- 
+-- Fix InnoDB statistics with almost no impact on a live server
+-- 
+-- @link https://www.percona.com/blog/2017/09/11/updating-innodb-table-statistics-manually/
+-- 
+```php
+$ php -r '
+$link = new mysqli("127.0.0.1", "root", "", "test", 13001);
+$link->query("set lock_wait_timeout=1");
+while(!$link->query("lock table goods write")) {sleep(1);}
+$link->query("flush table goods");
+$link->query("unlock tables");'
+```
