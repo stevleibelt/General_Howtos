120cb746412b6cb52a280720e958f6a7a4405688
stevleibelt <stev@leibelt.de>
2018-02-07 09:39:22 +0100

    added

---
+++ development/mysql/order_by.sql
@@ -0,0 +1,5 @@
+-- 
+-- @see
+--  https://www.percona.com/blog/2018/02/05/four-ways-to-execute-mysql-group-by/
+--  https://www.percona.com/blog/2018/01/29/using-generated-columns-in-mysql-5-7-to-increase-query-performance/
+-- 
