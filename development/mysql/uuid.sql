2cb7978451f17980fc77f140bbaed4de8d523936
stevleibelt <stev@leibelt.de>
2018-04-11 15:08:15 +0200

    added uuid

---
+++ development/mysql/uuid.sql
@@ -0,0 +1,10 @@
+-- 
+-- @see
+--  https://www.percona.com/blog/2007/03/13/to-uuid-or-not-to-uuid/
+--  https://www.percona.com/blog/2014/12/19/store-uuid-optimized-way/
+--  https://dev.mysql.com/doc/refman/5.5/en/miscellaneous-functions.html#function_uuid
+--  http://kccoder.com/mysql/uuid-vs-int-insert-performance/
+--  https://stackoverflow.com/questions/2365132/uuid-performance-in-mysql
+--
+
+SELECT UUID();
