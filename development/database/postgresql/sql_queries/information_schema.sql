159b832c9c6654cc05e617ace287382cbffac942
stev leibelt <artodeto@bazzline.net>
2022-07-08 15:21:22 +0200

    fixed issue

--- development/database/postgresql/sql_queries/information_schema.sql
+++ development/database/postgresql/sql_queries/information_schema.sql
@@ -19,7 +19,7 @@ WHERE
     c.column_name LIKE '%<search_pattern>%'
     -- table name is optional
     AND t.table_name = '<table_name>'
-    AND t.table_schema NOT INT ('information_schema', 'pg_catalog')
+    AND t.table_schema NOT IN ('information_schema', 'pg_catalog')
     AND t.table_type = 'BASE TABLE'
 ORDER BY
     t.table_schema;
