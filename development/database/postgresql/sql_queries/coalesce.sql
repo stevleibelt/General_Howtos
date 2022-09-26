15085e72fbbe13919d9ee007cae46791a0b568ed
stev leibelt <artodeto@bazzline.net>
2022-09-26 10:58:46 +0200

    extended examples for coalesce, started string example and added does and donts in the postgresql readme

--- development/database/postgresql/sql_queries/coalesce.sql
+++ development/database/postgresql/sql_queries/coalesce.sql
@@ -2,21 +2,45 @@
 -- @see
 --  https://www.postgresql.org/docs/14/functions-conditional.html#FUNCTIONS-COALESCE-NVL-IFNULL
 -- 
+-- @tags/search patterns
+--  String is empty | empty string
+-- 
 
+-- 
 -- I've used it for the case where I need to join two tables and the content of a column
 --  was allowed to be '$value', '' or NULL
 -- Furthermore it is used to output/display null values as human readable '(none)' or 'NO DATA'
+
+-- Compare two columns that could contain '' or NULL
+--  If a column value either is '' or NULL, it should be be handled as equal
+-- 
 
 SELECT
     foo.baz,
     bar.baz
 FROM
-    foo
-    INNER JOIN bar
+    public.foo
+    INNER JOIN public.bar
     ON (
         -- CAUTION, baz could contain a value, '' or NULL
         --  '' and NULL are seen as empty
         COALESCE(foo.baz, '') = COALESCE(bar.baz, '')
     )
 ;
+
+-- 
+-- Check if column value is not empty
+-- 
+
+-- By using TRIM
+--  TRIM should be used if you deal with a string that can contain only spaces
+SELECT foo.bar
+FROM public.foo 
+WHERE TRIM(foo.bar) != '';
+
+-- By using COALESCE
+SELECT foo.bar
+FROM public.foo 
+WHERE COALESCE(foo.bar, '') != '';
+
 
