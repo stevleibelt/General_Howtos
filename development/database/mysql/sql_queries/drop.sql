a4c3e28914b4fff79e08bf3c44df07c6ce8169a6
stev leibelt <artodeto@bazzline.net>
2022-10-06 07:37:58 +0200

    updated

--- development/database/mysql/sql_queries/drop.sql
+++ development/database/mysql/sql_queries/drop.sql
@@ -7,3 +7,11 @@ DROP TABLE [IF EXISTS] `my_table`
 -- delete database
 -- 
 DROP DATABASE [IF EXISTS] `my_database`
+
+-- 
+-- delete user if "delete" is not working
+-- @see
+--  https://www.percona.com/blog/when-manipulating-mysql-user-tables-goes-wrong-troubleshooting-error-1396/ - 20221006T07:37:20
+-- 
+DROP USER [IF EXISTS] `my_user`
+
