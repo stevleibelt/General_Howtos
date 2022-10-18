24ba8a85a0886638a4c4a57fec66acb11df8df01
stev leibelt <artodeto@bazzline.net>
2022-10-18 15:02:58 +0200

    added postgresql how to format a number

--- development/database/postgresql/sql_queries/string.sql
+++ development/database/postgresql/sql_queries/string.sql
@@ -22,3 +22,17 @@ SELECT CONCAT_WS('-', 'There', 'is', 'no', NULL, 'foo', 'without', 'a', 'bar');
 -- Trim
 SELECT '----' || TRIM(' there is no foo without a bar    ') || '----';
 
+-- Check that string only contains numbers and format then
+WITH my_values (my_string) AS (
+    VALUES
+        ('01'),
+        ('2014'),
+        ('2014-06-25'),
+        ('Text')
+)
+SELECT
+    my_values.my_string AS original_string,
+    CASE WHEN my_values.my_string ~'^[0-9]+$' THEN my_values.my_string::DECIMAL ELSE 'NaN' END AS optional_decimal_number,
+        CASE WHEN my_values.my_string ~'^[0-9]+$' THEN TO_NUMBER(my_values.my_string, 'FM999') ELSE 'NaN' END AS optional_formatted_number
+FROM my_values;
+
