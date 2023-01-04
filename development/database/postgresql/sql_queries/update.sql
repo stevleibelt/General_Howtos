5bdc1b5ff5da2616b1dcdf168ee70dc8dde9187a
stev leibelt <artodeto@bazzline.net>
2023-01-04 13:56:09 +0100

    wrapped update with a transaction

--- development/database/postgresql/sql_queries/update.sql
+++ development/database/postgresql/sql_queries/update.sql
@@ -1,14 +1,19 @@
 -- 
 -- @see
 --  https://www.postgresql.org/docs/current/sql-update.html
+--  https://www.postgresql.org/docs/current/tutorial-transactions.html
 -- 
 
-UPDATE public.my_table
-SET public.my_table.column_one = 'there is no foo without a bar'
-FROM other_schema.mo_table  -- FROM is optional, see it as the first join
-  JOIN another_one.mi_table  -- Of course you can join gazillion tables
-    ON another_one.mi_table_column_one = other_schema.mo_table.column_one
+BEGIN;
+
+UPDATE my_table
+SET column_one = 'there is no foo without a bar'
+FROM mo_table  -- FROM is optional, see it as the first join
+  JOIN mi_table  -- Of course you can join gazillion tables
+    ON mi_table_column_one = mo_table.column_one
 WHERE
-  public.my_table.column_two = other_schema.mo_table.column_two -- Since from is the first join, this is the fitting on-join-condition
-  AND another_one.mi_table_column_two = 'rewind selecta!';
+  my_table.column_two = mo_table.column_two -- Since from is the first join, this is the fitting on-join-condition
+  AND mi_table_column_two = 'rewind selecta!';
+
+COMMIT;
 
